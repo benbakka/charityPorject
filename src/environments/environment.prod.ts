7ab4ce0137a620d351b93c4a970f003dfdf2a114
AHMAMAAMINE <>
2025-02-02 19:29:51 +0100

    switch link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // apiUrl: 'https://charitybackend.onrender.com/api'  // Change this to your production API URL
-  apiUrl:'http://144.126.140.108/api/api'
+  apiUrl:'http://localhost/api/api'
 };
